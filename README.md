b7b4c51a7bcce74b8d34f3af9a7c6cc842f1cdfc
Stuart Saltzman <stuart.saltzman@gmail.com>
2018-02-09 08:30:12 -0500

    dockerize script

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Basic web app demonstrating how to containerise an ASP.NET Core app.
 
 Creates a basic app in a docker image by connecting the GitHub repository to Doker Hub
+
+# Run it
+```
+./docker_build.sh
+docker run -p 8080:80 aspnetcore-in-docker:latest
+```
--- docker_build.sh
+++ docker_build.sh
@@ -3,7 +3,9 @@ DOCKER_ORG=stuartsaltzman
 GITSHA1=${GITSHA1:=$(git rev-parse HEAD)}
 DOCKER_IMAGE_VERSION=${BUILD_NUMBER}.${GITSHA1}
 
-docker build -t $DOCKER_ORG/aspnetcore-in-docker:$DOCKER_IMAGE_VERSION .
+#docker build -t $DOCKER_ORG/aspnetcore-in-docker:$DOCKER_IMAGE_VERSION .
+docker build -t aspnetcore-in-docker:$DOCKER_IMAGE_VERSION .
 
 # tag image with latest tag
-docker tag $DOCKER_ORG/aspnetcore-in-docker:$DOCKER_IMAGE_VERSION $DOCKER_ORG/aspnetcore-in-docker:latest
+#docker tag aspnetcore-in-docker:$DOCKER_IMAGE_VERSION $DOCKER_ORG/aspnetcore-in-docker:latest
+docker tag aspnetcore-in-docker:$DOCKER_IMAGE_VERSION aspnetcore-in-docker:latest
